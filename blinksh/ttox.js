e31623e0f8c316502bfd0e30a3225fef005a734a
Tomas <tomasruud@users.noreply.github.com>
2024-02-06 17:40:41 +0100

    Update ttox.js

--- blinksh/ttox.js
+++ blinksh/ttox.js
@@ -1,3 +1,31 @@
+black = "#3E3635";
+red = "#BD4E36";
+green = "#617934";
+yellow = "#896D35";
+blue = "#4C6EBF";
+magenta = "#9856B1";
+cyan = "#4C6EBF";
+white = "#D6D4D4";
+lightBlack = "#655E5C";
+lightRed = "#F4AB9B";
+lightGreen = "#8DCA61";
+lightYellow = "#DDB64F";
+lightBlue = "#A2B6EE";
+lightMagenta = "#C2B1E8";
+lightCyan = "#70CEB2";
+lightWhite = "#EEEDED";
+
+dark = {
+  fg: "#FFFFFF",
+  bg: "#1D1D1D",
+  cursor: "rgba(255, 255, 255, 0.5)",
+};
+
+light = {
+  fg: "#080808",
+  bg: "#FFFFFF",
+  cursor: "rgba(0, 0, 0, 0.5)",
+};
 
 t.prefs_.set("color-palette-overrides", [
   black,
